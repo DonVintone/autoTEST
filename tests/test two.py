aa7b38501d57882f4536cd9bb4fbf9873553af5e
Dandik <Da_nil@inbox.ru>
2023-01-11 22:09:29 +0300

    first commit

---
+++ tests/test two.py
@@ -0,0 +1,14 @@
+
+
+def test():
+    assert 1 in [2, 3, 4]
+
+    
+def test_2():
+    a=1
+    b=2
+    assert a<b
+
+
+def test_3():
+    assert 'fizz' not in 'fizzbuzz'
---
+++ tests/test_exceptions.py
@@ -0,0 +1,9 @@
+"""проверка на ожидание исключения"""
+
+import pytest
+
+def test_add_raises():
+     with pytest.raises(ZeroDivisionError):
+        1/0
+
+
